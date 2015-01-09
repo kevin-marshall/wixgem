f94581fc289e7d24f3e89b4334b795a14b13715d
Kevin Marshall <kevin@marshallville.net>
2015-01-09 08:18:29 -0700

    commit all

--- spec/test_install.rb
+++ spec/test_install.rb
@@ -9,7 +9,7 @@ def execute(cmd)
 end
 
 def admin?
-  %x[net session]
+  `net session`
   return true if $?.to_i==0
   return false
 end
@@ -20,22 +20,28 @@ def get_product_name(msi_file, arg2)
 end
 
 def test_msi(msi_file, arg2)
-    product_name = get_product_name(msi_file, arg2)
+  product_name = get_product_name(msi_file, arg2)
 	
-	msi_info = WindowsInstaller.msi_records(msi_file)
+  msi_info = WindowsInstaller.msi_records(msi_file)
+  #puts msi_info.to_s
+  
+  if(arg2.kind_of?(Hash) && arg2.has_key?(:product_name))
+    raise "ProductName is #{msi_info['ProductName']} expected #{product_name}" unless(product_name == msi_info['ProductName'])
+  end
 
-	if(arg2.kind_of?(Hash) && arg2.has_key?(:product_name))
-	  raise "ProductName is #{msi_info['ProductName']} expected #{product_name}" unless(product_name == msi_info['ProductName'])
-	end
-
-	if(arg2.kind_of?(Hash) && arg2.has_key?(:product_code))
-	  expected = arg2[:product_code].upcase
-	  raise "ProductCode is #{msi_info['ProductCode']} expected #{expected}" unless(expected == msi_info['ProductCode'])
-	end
+  if(arg2.kind_of?(Hash) && arg2.has_key?(:product_code))
+    expected = arg2[:product_code].upcase
+	raise "ProductCode is #{msi_info['ProductCode']} expected #{expected}" unless(expected == msi_info['ProductCode'])
+  end
 	
-	expected_product_version = '1.0.0.0'
-    expected_product_version = arg2[:version] if(arg2.kind_of?(Hash) && arg2.has_key?(:version))
-    raise "Invalid product version #{msi_info['ProductVersion']}" if(msi_info['ProductVersion'] != expected_product_version)
+  expected_product_version = '1.0.0.0'
+  expected_product_version = arg2[:version] if(arg2.kind_of?(Hash) && arg2.has_key?(:version))
+  raise "Invalid product version #{msi_info['ProductVersion']}" if(msi_info['ProductVersion'] != expected_product_version)
+
+  expected_manufacturer = 'Not Set'
+  expected_manufacturer = arg2[:manufacturer] if(arg2.kind_of?(Hash) && arg2.has_key?(:manufacturer))
+  puts "#{expected_manufacturer} = #{msi_info['Manufacturer']} = #{msi_info['Manufacturer'] != expected_manufacturer}"
+  raise "Invalid Manufacturer #{msi_info['Manufacturer']}" if(msi_info['Manufacturer'] != expected_manufacturer)
 end
 
 def test_install(name, msi_file, arg2)

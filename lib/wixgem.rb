0231f9d0f5ba5db0e70a5bcab756160bd5d07e6b
kevin-marshall <kevin@marshallville.net>
2015-01-12 12:23:45 -0700

    commit all

--- lib/wixgem.rb
+++ lib/wixgem.rb
@@ -124,16 +124,17 @@ class Wix
 	end
 
     if(@debug)	
-	  longest_path = files.max { |a, b| a.length <=> b.length }
+	  max_path = files.max { |a, b| a.length <=> b.length }
+	  columen_size = max_path.length + 10
 	  File.open('./installation_files.txt', 'w') do |f| 
-	    f.printf("%-#{longest_path.length}s %s" % ['File path', 'Installation Path'])
+	    f.printf("%-#{columen_size}s %s\n" % ['File path', 'Installation Path'])
 	    files.each do |file| 
 	      if(File.file?(file))
    	        install_path = file
             if(input.kind_of?(Hash) && input.has_key?(:modify_file_paths))
               input[:modify_file_paths].each { |regex, replacement_string| install_path = install_path.gsub(regex, replacement_string) }
             end
-	        f.printf("%-#{longest_path.length}s %s" % [file, install_path])
+	        f.printf("%-#{columen_size}s %s\n" % [file, install_path])
           end
 		end
 	  end

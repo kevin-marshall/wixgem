a6a2e88a3c7df77064290c0a06f0dfd8b6f90ad4
kevin-marshall <kevin@marshallville.net>
2015-01-12 16:07:20 -0700

    commit all

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,14 @@
 require 'dev_tasks'
 require './admin.rb'
 
+WIX_VERSION='3.9'
+
 CLEAN.include('test')
 
-WIX_DEP_PATH='OpenSource/WixToolset/3.9'
 DEV[:files][:source].include('test_files/**/*')
-DEV[:svn_exports][WIX_DEP_PATH] = 'https://deps.googlecode.com/svn/trunk/WixToolset/3.9'
+DEV[:svn_exports]["OpenSource/WixToolset/#{WIX_VERSION}"] = "https://deps.googlecode.com/svn/trunk/WixToolset/#{WIX_VERSION}"
 
-WIX_PATH = "#{Environment.dev_root}/dep/#{WIX_DEP_PATH}"
+WIX_PATH = "#{Environment.dev_root}/dep/OpenSource/WixToolset/#{WIX_VERSION}"
 
 Text.replace_text_in_glob './spec/wixpath.rb',/WIX_PATH='.+'/,"WIX_PATH='#{WIX_PATH}'"
 

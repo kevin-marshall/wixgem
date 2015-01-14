571ffea2ded09f95d120c518112216de0ca9af10
kevin-marshall <kevin@marshallville.net>
2015-01-14 12:18:26 -0700

    commit all

--- rakefile.rb
+++ rakefile.rb
@@ -13,9 +13,9 @@ WIX_PATH = "#{Environment.dev_root}/dep/OpenSource/WixToolset/#{WIX_VERSION}"
 Text.replace_text_in_glob './spec/wixpath.rb',/WIX_PATH='.+'/,"WIX_PATH='#{WIX_PATH}'"
 
 unless(admin?)
-puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-puts 'Running as non administrator will not be able to run the installer'
-puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+puts 'Running as non administrator. Will not be able to run the installer!'
+puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 end
 
 # Yard command line for realtime feed back of Readme.md modifications

1ac8873c70bfe9fdd80297d12268261c3641ac93
Kevin Marshall <kevin@marshallville.net>
2015-08-27 07:09:50 -0600

    'all'

--- testComObject.rb
+++
@@ -1,8 +0,0 @@
-require 'win32ole'
-
-object = WIN32OLE.new('COMObject.ComClassExample')
-puts "Object: #{object}"
-puts "Text: #{object.GetText}"
-object1 = WIN32OLE.new('{863AEADA-EE73-4f4a-ABC0-3FB384CB41AA}')
-puts "Object: #{object}"
-puts "Text: #{object.GetText}"

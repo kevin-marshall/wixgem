f44ae0b9cd4caa9d9ff431110b4045afc847d9e8
kevin-marshall <kevin@marshallville.net>
2015-11-10 13:09:07 -0700

    'all'

--- example/rakefile.rb
+++ example/rakefile.rb
@@ -1,7 +1,7 @@
 require 'wixgem'
 	
-Wixgem::Wix.install_path = 'C:\Development\dep\OpenSource\WixToolset\3.9'
-#Wixgem::Wix.install_path = '<Path to the root directory of the wix toolset>'
+#Wixgem::Wix.install_path = 'C:\Development\dep\OpenSource\WixToolset\3.9'
+Wixgem::Wix.install_path = '<Path to the root directory of the wix toolset>'
 
 task :create_installation_files do
   FileUtils.mkpath('./install_files/directory')
@@ -20,7 +20,7 @@ desc "Generate an installation msi file"
 task :installation => [:mergemodule] do	  
   installation_files = Dir.glob('./example.msm')
   Wixgem::Wix.make_installation("./example.msi",  
-    { upgrade_code: '{a62c35a7-6a6d-4392-822b-f6aca7eef88b}', debug: true,
+    { upgrade_code: '{a62c35a7-6a6d-4392-822b-f6aca7eef88b}',
 	  files: installation_files } 
   )
 end
